6a6c2b810008b3a4f192c165225bdc517fd37f30
ManuelNunja <nunja21@gmail.com>
2022-01-24 19:29:06 -0500

    commit_202201424_2

---
+++ src/components/FontAwesome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const FontAwesome = () => {
+  return (
+    <div>
+       <FontAwesomeIcon icon={faShoppingCart} />
+    </div>
+  )
+};
+
+export default FontAwesome;
